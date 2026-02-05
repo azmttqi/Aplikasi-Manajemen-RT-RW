e4a4e8efe2017c04ab90e95e5d0ee6c42bbd9591
azmttqi3 <azmiittaqi03@gmail.com>
2026-02-05 11:20:09 +0700

    Update API URL

--- frontend_flutter/lib/services/api_service.dart
+++ frontend_flutter/lib/services/api_service.dart
@@ -10,7 +10,7 @@ class ApiService {
   // ===========================================================================
   // - Pakai '10.0.2.2' jika pakai Emulator Android
   // - Pakai IP Laptop (misal '192.168.1.5') jika pakai HP Asli (Harus satu WiFi)
-  static const String baseUrl = "http://localhost:5000/api";
+  static const String baseUrl = "https://rtrw.demo.tazkia.ac.id/api";
 
   static const Duration _timeout = Duration(seconds: 15); // Batas waktu koneksi
 
